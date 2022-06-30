aa726c123b8b4ac443927106bd64533200ddcde3
Neo Mathekga <neozagm@gmail.com>
2022-06-30 14:42:58 -0700

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  *
  * Description: and that piece of art is useful - Dora Korpa
  *
- * return: ALways 0 for success
+ * Return: Always 0 (success)
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-return(1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
