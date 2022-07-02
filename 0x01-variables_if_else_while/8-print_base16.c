defa2f4a376cf4196d11a3d8a54879ff3cea68dc
Neo Mathekga <neozagm@gmail.com>
2022-07-01 19:41:42 -0700

    update 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,21 @@
-/**
- * File: 8-print_base16.c
- * Auth: Neo M
-*/
 #include <stdio.h>
 /**
- *main - entry point to the program 
- *
- * Description -it outputs all the numbers of base 16 in lower case
- *
- * Return: Always 0 for success
+ *main - main block
+ *Description: it outputs all the numbers of base 16 in lower case
+ *Return: 0
 */
 int main(void)
 {
 	int number;
 	char letter;
-
 	for (number = 0; number < 10; number++)
+	{
 		putchar((number % 10) + '0');
-
-	for (letter = 'a'; letter <= 'f'; letter++)
+	}
+		for (letter = 'a'; letter <= 'f'; letter++)
+	{
 		putchar(letter);
+	}
 	putchar('\n');
-
 	return (0);
 }
