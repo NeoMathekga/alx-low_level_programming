96b1ecc04f01497a18aab611017efd196338d540
Neo Mathekga <neozagm@gmail.com>
2022-07-05 08:02:54 -0700

    ./0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0 for success
+ *
+ * Description - prints putchar_ to the screen
+*/
+int main()
+{
+	printf("_putchar\n");
+	
+	return 0;
+}
