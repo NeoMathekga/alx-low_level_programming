dddb02412e8022983bf6f8da9629fcf581dd9c70
Neo Mathekga <Neozagm@gmail.com>
2022-07-12 01:31:16 +0200

    fix 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,9 @@
-#include <stdio.h>
 #include "main.h"
-#include "Holberton.h"
+
 /**
- * reset_to_98 - uses a pointer
- *@n: this is a pointer
- * Return: void
+ * reset_to_98 -> updateS the value in a pointer
+ * @n: pointer to n in func
  */
-void reset_to_98(int *n);
-
-void main(void);
-{
-	int i;
-
-	reset_to_98(&i)
-	_putchar("%d", i);
-}
-
 void reset_to_98(int *n)
 {
 	*n = 98;
