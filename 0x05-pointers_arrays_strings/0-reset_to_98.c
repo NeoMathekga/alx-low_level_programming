7f8474a902d4ff55fcc2c16707b4ada5188434f7
Neo Mathekga <neozagm@gmail.com>
2022-07-11 14:59:20 -0700

    sandbox update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include "main.h"
-#include "Holberton.h"
 /**
  * reset_to_98 - uses a pointer
  *@n: this is a pointer
  * Return: void
  */
-void reset_to_98(int *n);
-
-void main(void);
-{
-	int i;
-
-	reset_to_98(&i)
-	_putchar("%d", i);
-}
-
 void reset_to_98(int *n)
 {
 	*n = 98;
