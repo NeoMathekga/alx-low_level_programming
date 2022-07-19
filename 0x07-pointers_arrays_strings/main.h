587cc1db954ea8fb118485959cfc47d25199a047
Neo Mathekga <neozagm@gmail.com>
2022-07-18 21:57:04 -0700

    main update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,8 @@
  *function prototypes for all tasks
  *
  */
+
+void set_string(char **s, char *to);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
