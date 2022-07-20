8c8c6390e1cc0ce7749ef0e31e33ad2383cff7ad
Neo Mathekga <neozagm@gmail.com>
2022-07-20 09:13:10 -0700

    task 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,28 +1,19 @@
 #include "main.h"
 /**
- *_print_rev_recursion- prints string
- *@s: string to print in reverse
- *
- *Description: function prints string
- *pointed to by s in reverse
- *
- *Return: void
- */
+*_print_rev_recursion- prints string
+*@s: string to print in reverse
+*
+*Description: function prints string
+*pointed to by s in reverse
+*
+*Return: void
+*/
 void _print_rev_recursion(char *s)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-
 	if (*s)
 	{
-		i += j;
-		j++;
-		_print_rev_recursion(s + j);
-	}
-	else if (i > 0)
-	{
-		i--;
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(s - i);
+
 	}
 }
